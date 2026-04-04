8c45fd6f54fcfa6c68ada610c40fa49e4ff630ee
Vivek Kustwar <developervick@gmail.com>
2026-04-04 22:46:29 +0530

    setups is almost done, project is reday to start buildingthe features

--- orm/root/urls.py
+++ orm/root/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # For downloading schema
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('swagger/redoc', SpectacularRedocView.as_view(url_name='schema'), name="redoc"),
-    path('api/v1/', include('orm.urls')),
+    path('api/v1/', include('courses.urls')),
     path('api/v1/', include('users.urls')),
 ]
 
