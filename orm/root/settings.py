48d9834c0d38b8ef99275796a860b7f7e142185c
Vivek Kustwar <developervick@gmail.com>
2026-04-04 22:43:44 +0530

    changed app name from orm to courses

--- orm/root/settings.py
+++ orm/root/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
     #apps
     'users',
-    'orm',
+    'courses',
 ]
 
 MIDDLEWARE = [
--- orm/users/views.py
+++ orm/users/views.py
@@ -1,7 +1,6 @@
 from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.response import Response
 from root.custom_jwt import get_tokens_for_user
-from rest_framework_simplejwt.authentication import JWTTokenUserAuthentication
 
 class user():
     id = 1
